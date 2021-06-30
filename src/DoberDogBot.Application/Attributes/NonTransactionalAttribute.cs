4b92948fbf41c3d136b9d87de1eaa98fcc833f80
Sertay Kabuk <sertaykabuk@gmail.com>
2021-06-30 18:01:36 +0300

    NonTransactional command option added.

---
+++ src/DoberDogBot.Application/Attributes/NonTransactionalAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace DoberDogBot.Application.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
+    public class NonTransactionalAttribute : System.Attribute
+    {
+    }
+}
